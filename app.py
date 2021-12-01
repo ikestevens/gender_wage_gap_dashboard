320f7118f02b312821db8398c4d645522b83cd71
Isaac Stevens <isaacbstevens@gmail.com>
2021-11-30 21:33:28 -0700

    external

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 summary = '''
